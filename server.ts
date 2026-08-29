4eb67bf7b3a89977a1df8b6dd01be5452ce6b1e6
site3facil-a11y <site3facil@gmail.com>
2026-08-29 03:49:47 -0300

    chore: import fs module in server.ts

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import fs from 'fs';
 import { createServer as createViteServer } from 'vite';
 import {
   getPartners,
